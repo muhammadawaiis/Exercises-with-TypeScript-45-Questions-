d60e1aed4fb38f3760b0c5c121df822bea4df97a
Muhammed Awais <157340031+muhammadawaiis@users.noreply.github.com>
2024-04-01 11:32:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = () => Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "useRandomNumber",
+        type: 'number',
+        message: "Enter Any Random Number"
+    }]);
+if (answer.useRandomNumber === randomNumber) {
+    console.log("Congrulation You Enter Currect Number");
+}
+else {
+    console.log("Sorry This is  not currect number");
+}
